0acaae38fad1629c570b21c966b8e2cfae1f23a8
hahapiggy <58760348+hahapiggy@users.noreply.github.com>
2021-01-02 22:50:17 +0800

    update menu api

--- apis/update_menu.php
+++ apis/update_menu.php
@@ -2,12 +2,13 @@
     require("./utils.php");
     include_once "db_conn.php";
 
+    $number = $_POST["menu_ID"];
     $name = $_POST["menu_name"];
     $price = $_POST["price"];
 
-    $query = ("update `menu` set menu_name=?, price=? where menu_name=?");
+    $query = ("update `menu` set menu_name=?, price=? where menu_ID=?");
     $stmt = $db->prepare($query);
-    $result = $stmt->execute(array($menu_name, $price, $menu_name));
+    $result = $stmt->execute(array($menu_name, $price, $menu_ID));
     header("Location:inform.php");
     
     echo json_encode($result);
